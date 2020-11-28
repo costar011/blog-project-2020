d4bad688a96129902b7f4a2e24b37bbb739b07aa
YERIM <dnaer456@gmail.com>
2020-11-28 15:48:08 +0900

    desc

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
+import express from "express"; // express framework를 사용하기 위해 import함
 import dotenv from "dotenv";
 dotenv.config(); // 보안 코딩
-import morgan from "morgan";
+import morgan from "morgan"; // debugging을 위해 morgan을 import함
 
 const PORT = process.env.PORT;
 
@@ -11,6 +11,9 @@ const app = express();
 // app.js 에게 퍼그를 써야한다고 신호를 줌
 // view engine <- 키워드
 app.set("view engine", "pug");
+
+// app에게 morgan를 써야한다고 신호를 준다.
+// dev <- 키워드
 app.use(morgan(`dev`));
 
 // 설정 끝난 후 Server Start
