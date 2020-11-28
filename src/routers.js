940014993401278548c555011fb94b3d2541a32b
YERIM <dnaer456@gmail.com>
2020-11-28 16:06:16 +0900

    router

---
+++ src/routers.js
@@ -0,0 +1,10 @@
+// global router
+const HOME = "/";
+const CONTACT = `/contact`;
+
+// development router
+const DEVELOPMENT = `/development`;
+const NODEJS = `/nodejs`;
+const MONGODB = `/mongoDB`;
+const JAVASCRIPT = `/javascript`;
+const REACT = `/react`;
